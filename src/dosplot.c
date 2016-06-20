ee92e85715042c9dc9a6acaf5f2300d5650c430b
Emma Davenport <Davenport.Physics@gmail.com>
2016-06-20 14:42:09 -0500

    Updated version to .3

--- src/dosplot.c
+++ src/dosplot.c
@@ -33,7 +33,7 @@
 #include "totaldos.h"
 #include "partialdos.h"
 
-static char *ProgramVersion = ".22";
+static char *ProgramVersion = ".3";
 
 static void InitializeProgramState();
 static void MakeDosDirectories();
